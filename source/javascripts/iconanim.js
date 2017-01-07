bebe7fd69d5618a5baf211edeac083645f18d52b
hugoth <htessierhuort@gmail.com>
2017-01-07 18:28:23 +0100

    smoothscroll + test appear button.js

--- source/javascripts/iconanim.js
+++ source/javascripts/iconanim.js
@@ -9,18 +9,5 @@ $(document).ready(function (){
         $(this).addClass('animated fadeInUp');
         $(this).css('opacity', '1');
   }, { offset: '100%' });
-
   });
 });
-
-
-
-
-  $(document).ready(function() {
-    $('.scroll').on('click', function() { // Au clic sur un élément
-      var page = $(this).attr('#scrollcommand'); // Page cible
-      var speed = 2000; // Durée de l'animation (en ms)
-      $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-      return false;
-    });
-  });
