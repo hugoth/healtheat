c7aac7a3b19691f6c001a143cc6d7cf89b46f1e3
hugoth <htessierhuort@gmail.com>
2017-02-13 01:21:08 +0100

    add leon, delete partial pain and brigitte

--- source/javascripts/snipcart.js
+++ source/javascripts/snipcart.js
@@ -1,7 +1,7 @@
-// Snipcart.execute('bind', 'cart.opened', function() {
-//     Snipcart.execute('unbind', 'cart.opened');
+Snipcart.execute('bind', 'cart.opened', function() {
+    Snipcart.execute('unbind', 'cart.opened');
 
-//     var html = $("#cart-content-text").html();
-//     $(html).insertBefore($("#snipcart-footer"));
-// });
+    var html = $("#cart-content-text").html();
+    $(html).insertBefore($("#snipcart-footer"));
+});
 
